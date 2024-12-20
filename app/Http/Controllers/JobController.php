508a21f2de86a8ea46516a528f1c048613b90117
Abhinav <abhinav4848@gmail.com>
2024-12-20 16:18:34 +0000

    simplifying routes

---
+++ app/Http/Controllers/JobController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use Illuminate\Http\Request;
+
+class JobController extends Controller
+{
+    // the following are called actions on a controller
+    public function index()
+    {
+        //$jobs = Job::all(); //prevent N+1 database querying problem; when it's used in a context where each Job item requires additional data, especially if it's loading related models within a loop.
+        //We can prevent lazy loading entirely in the application by going to Appserviceprovider.php and disabling it, so anytime any code in your application tries to lazy load anything, it'll give an error saying lazy loading is disabled so you can go back and tweak that code to make it only eager load.
+        $jobs = Job::with('employer')->latest()->simplePaginate(3);
+        //Paginate tells how many pages exist
+        //simplePaginate saves on calculating how many pages there are- useful if many entries exist.
+        //cursorPaginate makes url gibberish
+
+        return view('jobs.index', [
+            'jobs' => $jobs
+        ]);
+    }
+
+    public function create()
+    {
+        return view('jobs.create');
+    }
+
+    public function show(Job $job)
+    {
+        return view('jobs.show', ['job' => $job]);
+
+    }
+
+    public function store()
+    {
+        // validation
+        request()->validate([
+            'title' => ['required','min:3'],
+            'salary' => ['required']
+        ]);
+
+        Job::create([
+            'title' => request('title'),
+            'salary' => request('salary'),
+            'employer_id' => 1
+        ]);
+
+        return redirect('/jobs');
+    }
+
+    public function edit(Job $job)
+    {
+        // $job = Job::find($id);
+        return view('jobs.edit', ['job' => $job]);
+    }
+
+    public function update(Job $job)
+    {
+        // authorize (on hold...)
+
+        //validate
+        request()->validate([
+            'title' => ['required','min:3'],
+            'salary' => ['required']
+        ]);
+
+        //get the job to update
+        // $job = Job::findOrFail($id); // else there'd be a problem if the job didn't exist and find returned null then we called update on it.
+
+        //update the job
+        $job->update([
+            'title' => request('title'),
+            'salary' => request('salary'),
+        ]);
+
+        // Same as the update method
+        // $job->title = request('title');
+        // $job->salary = request('salary');
+        // $job->save();
+
+        //redirect to the job page
+        return redirect('/jobs/'.$job->id);
+    }
+
+    public function destroy(Job $job)
+    {
+        // authorize
+
+        // delete
+        $job->delete();
+
+        // Job::findOrFail($id)->delete(); // else there'd be a problem if the job didn't exist and find returned null then we called update on it. // not using this anymore as we're calling object Job
+
+        //alternative
+        // $job = Job::findOrFail($id)->delete();
+        // $job->delete();
+
+        //redirect
+        return redirect('/jobs');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,109 +1,36 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
 
-Route::get('/', function () {
-    return view('home');
-});
+// php artisan route:list --except-vendor
+// See https://laracasts.com/series/30-days-to-learn-laravel-11/episodes/19
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
-// Display all jobs
-Route::get('/jobs', function () {
-    //$jobs = Job::all(); //prevent N+1 database querying problem; when it's used in a context where each Job item requires additional data, especially if it's loading related models within a loop.
-    //We can prevent lazy loading entirely in the application by going to Appserviceprovider.php and disabling it, so anytime any code in your application tries to lazy load anything, it'll give an error saying lazy loading is disabled so you can go back and tweak that code to make it only eager load.
-    $jobs = Job::with('employer')->latest()->simplePaginate(3);
-    //Paginate tells how many pages exist
-    //simplePaginate saves on calculating how many pages there are- useful if many entries exist.
-    //cursorPaginate makes url gibberish
+Route::resource('jobs', JobController::class); //this one assumes all routes exist even if they don't
 
-    return view('jobs.index', [
-        'jobs' => $jobs
-    ]);
-});
+//customize the above with only or except based on what routes you actually have
+// Route::resource('jobs', JobController::class, [
+//     'only' => ['index', 'show', 'edit']
+// ]);
 
-// Page to create a job
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
+// Otherwise go verbose
+// Route::controller(JobController::class)->group(function () {
+//     Route::get('/jobs', 'index');
+//     Route::get('/jobs/create', 'create');
+//     Route::get('/jobs/{job}', 'show');
+//     Route::post('/jobs', 'store');
+//     Route::get('/jobs/{job}/edit', 'edit');
+//     Route::patch('/jobs/{job}', 'update');
+//     Route::delete('/jobs/{job}', 'destroy');
+// });
 
-// Shows a job. Since it's a wildcard, it's at the bottom of all jobs::GET routes
-// Wildcard routes should be at the bottom
-Route::get('/jobs/{job}', function (Job $job) {
-    // for laravel to interpret it as an ID, wildcard and parameter name to be identical because that is interpreted as {job:id}
-    // now it can just be job. See: https://laracasts.com/series/30-days-to-learn-laravel-11/episodes/19
-    // $job = Job::find($id);
-    return view('jobs.show', ['job' => $job]);
 
-});
-
-// Persist a job in the database
-Route::post('/jobs', function () {
-    // validation
-    request()->validate([
-        'title' => ['required','min:3'],
-        'salary' => ['required']
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1
-    ]);
-
-    return redirect('/jobs');
-});
-
-// Edit a job
-Route::get('/jobs/{job}/edit', function (Job $job) {
-    // $job = Job::find($id);
-    return view('jobs.edit', ['job' => $job]);
-});
-
-//Update a job
-Route::patch('/jobs/{job}', function (Job $job) {
-    // authorize (on hold...)
-
-    //validate
-    request()->validate([
-        'title' => ['required','min:3'],
-        'salary' => ['required']
-    ]);
-
-    //get the job to update
-    // $job = Job::findOrFail($id); // else there'd be a problem if the job didn't exist and find returned null then we called update on it.
-
-    //update the job
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary'),
-    ]);
-
-    // Same as the update method
-    // $job->title = request('title');
-    // $job->salary = request('salary');
-    // $job->save();
-
-    //redirect to the job page
-    return redirect('/jobs/'.$job->id);
-});
-
-// Destroy
-Route::delete('/jobs/{job}', function (Job $job) {
-    // authorize
-
-    // delete
-    $job->delete();
-
-    // Job::findOrFail($id)->delete(); // else there'd be a problem if the job didn't exist and find returned null then we called update on it. // not using this anymore as we're calling object Job
-
-    //alternative
-    // $job = Job::findOrFail($id)->delete();
-    // $job->delete();
-
-    //redirect
-    return redirect('/jobs');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
+// Route::get('/jobs', [JobController::class, 'index']);
+// Route::get('/jobs/create', [JobController::class, 'create']);
+// Route::get('/jobs/{job}', [JobController::class, 'show']);
+// Route::post('/jobs', [JobController::class, 'store']);
+// Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
+// Route::patch('/jobs/{job}', [JobController::class, 'update']);
+// Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
