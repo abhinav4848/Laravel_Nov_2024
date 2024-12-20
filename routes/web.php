d4dbd1790fa09e4b1697f0b8a8c7a119b6456c48
Abhinav <abhinav4848@gmail.com>
2024-12-20 16:20:38 +0000

    starting authentication in a new project

--- routes/web.php
+++ routes/web.php
@@ -9,28 +9,3 @@ Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class); //this one assumes all routes exist even if they don't
-
-//customize the above with only or except based on what routes you actually have
-// Route::resource('jobs', JobController::class, [
-//     'only' => ['index', 'show', 'edit']
-// ]);
-
-// Otherwise go verbose
-// Route::controller(JobController::class)->group(function () {
-//     Route::get('/jobs', 'index');
-//     Route::get('/jobs/create', 'create');
-//     Route::get('/jobs/{job}', 'show');
-//     Route::post('/jobs', 'store');
-//     Route::get('/jobs/{job}/edit', 'edit');
-//     Route::patch('/jobs/{job}', 'update');
-//     Route::delete('/jobs/{job}', 'destroy');
-// });
-
-
-// Route::get('/jobs', [JobController::class, 'index']);
-// Route::get('/jobs/create', [JobController::class, 'create']);
-// Route::get('/jobs/{job}', [JobController::class, 'show']);
-// Route::post('/jobs', [JobController::class, 'store']);
-// Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-// Route::patch('/jobs/{job}', [JobController::class, 'update']);
-// Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
